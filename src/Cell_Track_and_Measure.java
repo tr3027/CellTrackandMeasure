4eb689f12a71e08e3bc97340b6144d63a03d95eb
Tom Ruzicka <T.Ruzicka@smc.at>
2022-11-14 21:56:34 +0100

    New features added
    + check correct cell tracked between frames (Issue #1)
    + config dialog for cell tracking
    + added the magic ROI "click" point into the overlay
    + selecting tracking channel switches image to that channel

--- src/Cell_Track_and_Measure.java
+++ src/Cell_Track_and_Measure.java
@@ -17,6 +17,7 @@ ClipboardOwner, KeyListener, */ {
 
     private static final String TITLE = "Cell Track and Measure";
     private static final String BTN_CMWT_CONFIG = "MagicWand config";
+    private static final String BTN_CTAM_CONFIG = "Tracking config";
     private static final String BTN_OVERLAY = "Track & overlay";
     private static final String BTN_MEASURE = "Track & measure";
     private static final String LBL_CHANNEL = "Select channel to track in:";
@@ -41,6 +42,16 @@ ClipboardOwner, KeyListener, */ {
     boolean done = false;
     private ImageCanvas previousCanvas = null;
 
+    /* maximum acceptable area difference of a ROI between two frames (1=100%) */
+    double cellAreaDiff = 0.1;
+
+    /* maximum acceptable mean difference of a ROI between two frames (1=100%) */
+    double cellMeanDiff = 0.1;
+
+    /* maximum distance from previous frame's ROI center to search for a cell in pixels.
+       The algorithm makes maximum this number of clock-wise turns around the original center. */
+    int cellCenterDistance = 2;
+
     public Cell_Track_and_Measure() {
         super(Cell_Track_and_Measure.TITLE); // set plugin window title
 
@@ -56,9 +67,10 @@ ClipboardOwner, KeyListener, */ {
                 setLayout(new FlowLayout(FlowLayout.CENTER,5,5));
 
                 panel = new Panel();
-                panel.setLayout(new GridLayout(10, 0, 0, 0));
+                panel.setLayout(new GridLayout(12, 0, 0, 0));
             
                 panel.add(myButton(Cell_Track_and_Measure.BTN_CMWT_CONFIG, true));
+                panel.add(myButton(Cell_Track_and_Measure.BTN_CTAM_CONFIG, true));
 
                 panel.add(new Label()); // just a spacer
                 
@@ -92,6 +104,25 @@ ClipboardOwner, KeyListener, */ {
         }
     } // Cell_Track_and_Measure.Cell_Track_and_Measure()
 
+	public void showConfigDialog() {
+        GenericDialog gd = new GenericDialog(Cell_Track_and_Measure.TITLE + " config");
+
+        gd.addMessage("Maximum cell differences between frames");
+		gd.addNumericField("Area: ", cellAreaDiff, 1);
+		gd.addNumericField("Mean: ", cellMeanDiff, 1);
+        gd.addMessage("");
+        gd.addNumericField("ROI search center distance", cellCenterDistance);
+
+        gd.setResizable(false);
+        gd.showDialog();
+        if (gd.wasCanceled()) return;
+
+        cellAreaDiff = (double) gd.getNextNumber();
+        cellMeanDiff = (double) gd.getNextNumber();
+        cellCenterDistance = (int) gd.getNextNumber();
+	} // Cell_Track_and_Measure.
+
+
     Button myButton(String label, boolean enabled) {
 		Button btn = new Button(label);
         btn.setEnabled(enabled);
@@ -116,6 +147,9 @@ ClipboardOwner, KeyListener, */ {
             switch (command) {
                 case Cell_Track_and_Measure.BTN_CMWT_CONFIG:
                     cmwt.showOptionsDialog();
+                    break;
+                case Cell_Track_and_Measure.BTN_CTAM_CONFIG:
+                    showConfigDialog();
                     break;
                 case Cell_Track_and_Measure.BTN_OVERLAY:
                     trackCells(true);
@@ -168,6 +202,10 @@ ClipboardOwner, KeyListener, */ {
 		}
 	} // Cell_Track_and_Measure.run()
 
+    private double percDiff(double oldValue, double newValue) {
+        return Math.abs((newValue - oldValue) / (oldValue));
+    } // Cell_Track_and_Measure.percDiff()
+
     /** Tracks the selected cells within the selected channel 
         and creates an overlay or measures the cells based 
         on the parameter <code>overlay</code>.
@@ -183,41 +221,95 @@ ClipboardOwner, KeyListener, */ {
             int channelCount = channelChoice.getItemCount();
             ResultsTable res = new ResultsTable();
             Overlay ovr = new Overlay();
-            double[] sum, sqSum;
+            double[] sum, sqSum, roiMean, roiArea;
+            int[] roiX, roiY;
 
             IJ.showStatus("Tracking "+rois.length+" cell"+(rois.length > 1 ? "s" : "")+" in "+frames+" frames ...");
 
-            for (int frame = 0; frame < frames; frame++) { // for each frame
-                IJ.showProgress(frame+1, frames);
+            for (int frameIndex = 0; frameIndex < frames; frameIndex++) { // for each frame
+                IJ.showProgress(frameIndex+1, frames);
+
                 // setPosition requires 1-based indices !
                 /* @TODO: maybe some day support for more then 1 slice could be added... */
-                imp.setPosition(channelChoice.getSelectedIndex()+1, 1, frame+1); 
+                imp.setPosition(channelChoice.getSelectedIndex()+1, 1, frameIndex+1); 
 
-                sum = new double[channelCount];
-                sqSum = new double[channelCount];
+                sum = new double[channelCount]; // sum of all ROI means within a frame for each channel
+                sqSum = new double[channelCount]; // Square sum of all ROI means within a frame for each channel
+                
+                // ROI data for each ROI in a single frame of the tracking channel
+                roiX = new int[rois.length]; 
+                roiY = new int[rois.length];
+                roiArea = new double[rois.length];
+                roiMean = new double[rois.length];
 
                 if (!overlay) res.incrementCounter();
 
                 for (var roiIndex = 0; roiIndex < rois.length; roiIndex++) { //for each ROI in the frame
                     Roi tmpRoi = rois[roiIndex];
                     imp.setRoi(tmpRoi);
-                    if (frame > 0) { // on all but 1st frame regenerate the ROI from the center of the ROI
-                        stat = imp.getStatistics(ij.measure.Measurements.CENTROID); // get current ROI statistics
-                        tmpRoi = cmwt.makeRoi(
-                            (int) Math.round(stat.xCentroid / calib.pixelWidth), 
-                            (int) Math.round(stat.yCentroid / calib.pixelHeight), 
-                            imp
-                        ); // regenerate the ROI from the center of the ROI
+                    // cet current ROIs statistics in the tracking channel
+                    stat = imp.getStatistics(ij.measure.Measurements.MEAN + ij.measure.Measurements.AREA + ij.measure.Measurements.CENTROID);
+                    roiX[roiIndex] = (int) Math.round(stat.xCentroid / calib.pixelWidth);
+                    roiY[roiIndex] = (int) Math.round(stat.yCentroid / calib.pixelHeight);
+                    roiMean[roiIndex] = stat.mean;
+                    roiArea[roiIndex] = stat.area;
+                    int x=0,y=0,row=0; // used to circle around the original ROI's center if needed
+                    if (frameIndex > 0) { // on all but 1st frame regenerate the ROI from the center of the ROI. In the first frame user created the ROI already!
+                        x=y=row=0; // used to circle around the original ROI's center if needed
+                        // while our ROI is not aproximately as huge and as bright as the previous ROI generate another ROI with it's center around 
+                        // previous ROI's center up to a defined max distance
+                        do {
+                            // generate new ROI in the centre of the previous frame's ROI
+                            tmpRoi = cmwt.makeRoi(
+                                roiX[roiIndex] + x, 
+                                roiY[roiIndex] + y, 
+                                imp
+                            ); // regenerate the ROI from the center of the ROI
+
+                            imp.setRoi(tmpRoi);
+                            stat = imp.getStatistics(ij.measure.Measurements.MEAN + ij.measure.Measurements.AREA);
+                            
+
+                            // are we in the limits for ROI difference between frames? => found our current frame ROI!
+                            /* @TODO: both limits should come as parameters from the GUI! */
+                            if ((percDiff(roiArea[roiIndex], stat.area) < cellAreaDiff) && (percDiff(roiMean[roiIndex], stat.mean) < cellMeanDiff)) {
+                                //IJ.log("Frame: "+(frameIndex+1)+", ROI: "+(roiIndex+1)+", offset: ["+x+","+y+"]");
+                                break;
+                            }
+
+                            // circle arround the original ROI's center CCW to find a better ROI on current frame
+                            if (row == 0 || (x == -1 && y == -row)) {
+                                row++;
+                                if (row < (cellCenterDistance+1)) {
+                                    x=0;
+                                    y=-row;
+                                }
+                            } else if (y == -row && x < row) {
+                                x++;
+                            } else if (x == row && y < row) {
+                                y++;
+                            } else if (y == row && x > -row) {
+                                x--;
+                            } else if (x == -row && y > -row) {
+                                y--;
+                            }
+
+
+                        } while (row < (cellCenterDistance+1));
+                        if (row == 3) IJ.log("Cell "+(roiIndex+1)+" not detected correctly in frame "+(frameIndex+1)+". Check the overlay!");
                         rois[roiIndex] = tmpRoi; // write the regenerated ROI back to our ROI array
                     }
                     for (int channelIndex = 0; channelIndex < channelCount; channelIndex++) { // apply the ROI to all channels
                         if (overlay) {
-                            tmpRoi.setPosition(channelIndex+1, 1, frame+1);
+                            tmpRoi.setPosition(channelIndex+1, 1, frameIndex+1);
                             ovr.add(tmpRoi);
+                            PointRoi tmpPoint = new PointRoi(roiX[roiIndex] + x, roiY[roiIndex] + y);
+                            tmpPoint.setPosition(channelIndex+1, 1, frameIndex+1);
+                            ovr.add(tmpPoint);
                         } else {
-                            imp.setPosition(channelIndex+1, 1, frame+1);
+                            imp.setPosition(channelIndex+1, 1, frameIndex+1);
                             imp.setRoi(tmpRoi);
-                            stat = imp.getStatistics(ij.measure.Measurements.MEAN);
+                            stat = imp.getStatistics(ij.measure.Measurements.MEAN + ij.measure.Measurements.AREA);
                             res.addValue("Cell "+(roiIndex+1)+": "+channelChoice.getItem(channelIndex), stat.mean);
                             sum[channelIndex] += stat.mean;
                             sqSum[channelIndex] += (stat.mean * stat.mean);
@@ -246,7 +338,7 @@ ClipboardOwner, KeyListener, */ {
             } else {
                 res.show("Trace data");
                 if (plotData.getState()) { // a graph was requested as well
-
+                    /* @TODO */
                 }
             }
         } else {
@@ -267,8 +359,8 @@ ClipboardOwner, KeyListener, */ {
     } // Cell_Track_and_Measure.mouseClicked()
 
     public void itemStateChanged(ItemEvent e) {
-        //IJ.showMessage(e.getItem());
-
+        ImagePlus imp = WindowManager.getCurrentImage();
+        imp.setC(channelChoice.getSelectedIndex()+1);
     }
 
 } // class Cell_Track_and_Measure
